9ce72f2408d58886855faa2cde4c4c4dd6dcb41e
Franklyn Reyes <franklynreyes777@yahoo.com>
2021-04-19 13:26:37 -0400

    included family driving miles and destination

--- src/main/java/com/tts/Car.java
+++ src/main/java/com/tts/Car.java
@@ -2,22 +2,71 @@ package com.tts;
 
 public class Car {
 
-    public int numberOfDoors;
-    public String color;
-    public String model;
+    private int numberOfDoors;
+    private String color;
+    private String model;
+    private int currentRPM;
+    private int speed;
 
-    public void start(){
-        System.out.println("Car starting");
+
+//    public static int convertGrossWeightToKGs(double lbs)
+//    {
+//        double grossWeightInLbs = 4000;
+//        double kgs = grossWeightInLbs/2.2 ;
+//        return (int)kgs;
+//    }
+
+
+
+
+    private int calculateRPM(int speed){
+        int rpm = 0;
+
+        return rpm;
+
+
     }
 
-    public void stop(){
-        System.out.println("Car stopping");
-    }
-    public void drive(){
-        System.out.println("Car driving");
+    public int getCurrentRPM() {
+        return currentRPM;
     }
 
-    // constructor with arguments
+    public void setCurrentRPM(int rpm) {
+        this.currentRPM = rpm;
+    }
+
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+        setCurrentRPM(calculateRPM(speed));
+    }
+
+    public String getColor (){
+        return color;
+    }
+    public void setColor(String color){
+        this.color = color;
+    }
+
+    public int getNumberOfDoors() {
+        return numberOfDoors;
+    }
+
+    public void setNumberOfDoors(int numberOfDoors) {
+        if ( numberOfDoors >= 1 && numberOfDoors <= 6)
+            this.numberOfDoors = numberOfDoors;
+        else
+            this.numberOfDoors = 1;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
     public Car(String color, int doors, String model)
     {
         this.color = color;
@@ -39,6 +88,51 @@ public class Car {
         this.color = color;
         this.numberOfDoors = 4;
     }
+
+    public Car(String color, int doors)
+    {
+        this.color = color;
+        this.numberOfDoors = doors;
+    }
+
+
+
+    public void start(){
+        System.out.println("Car starting");
+
+    }
+
+    public void stop(){
+        System.out.println("Car stopping");
+    }
+
+
+    // Just start driving
+    public void drive()
+    {
+        System.out.println("Car driving");
+    }
+
+    // Drive for the specified number of miles
+    public void drive(int miles)
+    {
+        System.out.println("Driving for " + miles + " miles.");
+    }
+
+    // Drive specified miles at the specified speed
+    public void drive(int miles, int speed)
+    {
+        System.out.println("Driving " + speed + "mph for " + miles + " miles.");
+    }
+
+    // drive to the specified destination
+    public void drive(String destination)
+    {
+        System.out.println("Driving to " + destination);
+    }
+
+    // constructor with arguments
+
 
 
 
--- src/main/java/com/tts/Main.java
+++ src/main/java/com/tts/Main.java
@@ -4,7 +4,18 @@ import java.util.ArrayList;
 
 class Main {
 
+    public static class InstanceCounter{
+        private static int numInstances = 0;
+    }
+
+
+
     public static void main(String[] args) {
+
+        System.out.println("==== Static Class ====");
+        System.out.println(InstanceCounter.numInstances);
+
+
 
         System.out.println("==== Dot Method=====");
         ArrayList<String> theList = new ArrayList<>();
@@ -34,23 +45,53 @@ class Main {
         car.stop();
         car.drive();
 
-        System.out.println(car.color);
-        System.out.println(car.numberOfDoors);
-        System.out.println(car.model);
+//        System.out.println(car.color);
+//        System.out.println(car.numberOfDoors);
+//        System.out.println(car.model);
+//
+//
+//
+//        System.out.println("==== Car 1 ===");
+//        Car car1 = new Car();
+//        System.out.println(car1.color);
+//        System.out.println(car1.numberOfDoors);
+//
+//
+//        System.out.println("===== Car 2 ======");
+//        Car car2 = new Car("Red");
+//
+//        System.out.println(car2.color);
+//        System.out.println(car2.numberOfDoors);
+
+        System.out.println("=== Class modified ===");
+        Car sporty = new Car("Red",2);
+        sporty.start();
+        sporty.drive();
+
+        System.out.println();
+
+        Car family = new Car();
+        family.setColor("Gold");
+        family.setNumberOfDoors(4);
+        family.start();
+        family.drive();
+        System.out.println(family.getColor());
+        System.out.println(family.getNumberOfDoors());
+        System.out.println();
+
+        System.out.println("====Overloading===");
+
+        family.drive();
+        family.drive(300);
+        family.drive(600,75);
+        family.drive("California");
 
 
 
-        System.out.println("==== Car 1 ===");
-        Car car1 = new Car();
-        System.out.println(car1.color);
-        System.out.println(car1.numberOfDoors);
 
 
-        System.out.println("===== Car 2 ======");
-        Car car2 = new Car("Red");
 
-        System.out.println(car2.color);
-        System.out.println(car2.numberOfDoors);
+
 
 
 
